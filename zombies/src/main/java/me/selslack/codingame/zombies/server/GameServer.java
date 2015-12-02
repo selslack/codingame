40de1cfbb7dacdd4d4c8dd66b2a2a7a756691d73
selslack <selslack@users.noreply.github.com>
2015-12-02 17:48:45 +0300

    GameServer concurrency improvements Added 2 test maps to GameServer

--- zombies/src/main/java/me/selslack/codingame/zombies/server/GameServer.java
+++ zombies/src/main/java/me/selslack/codingame/zombies/server/GameServer.java
@@ -5,11 +5,12 @@ import me.selslack.codingame.zombies.GameState;
 import me.selslack.codingame.zombies.Player;
 import me.selslack.codingame.zombies.mcts.Config;
 
+import java.util.*;
 import java.util.concurrent.*;
+import java.util.stream.IntStream;
 
 public class GameServer {
-    static final private ForkJoinPool executor = new ForkJoinPool(1);
-    static final public GameState[] cases = new GameState[3];
+    static final public GameState[] cases = new GameState[4];
 
     static {
         // Case 1: Simple
@@ -46,18 +47,45 @@ public class GameServer {
             .addZombie(13, 9000, 3000)
             .addZombie(14, 9000, 4000)
             .addZombie(15, 9000, 5000)
+            .addZombie(16, 10000, 1000)
+            .addZombie(17, 10000, 5000)
+            .addZombie(18, 11000, 1000)
+            .addZombie(19, 11000, 3000)
+            .addZombie(20, 11000, 5000)
+            .addZombie(21, 12000, 1000)
+            .addZombie(22, 12000, 3000)
+            .addZombie(23, 12000, 4000)
+            .addZombie(24, 12000, 5000)
+            .addZombie(25, 13000, 1000)
+            .addZombie(26, 13000, 3000)
             .state;
-    }
 
-    static public GameTask run(GameState state, Config config) {
-        return (GameTask) executor.submit(new GameTask(state, config));
+        // Case 7: combo opportunity
+        cases[3] = new GameStateBuilder(500, 4500)
+            .addHuman(0, 100, 4000)
+            .addHuman(1, 130, 5000)
+            .addHuman(2, 10, 4500)
+            .addHuman(3, 500, 3500)
+            .addHuman(4, 10, 5500)
+            .addHuman(5, 100, 3000)
+            .addZombie(0, 8000, 4500)
+            .addZombie(1, 9000, 4500)
+            .addZombie(2, 10000, 4500)
+            .addZombie(3, 11000, 4500)
+            .addZombie(4, 12000, 4500)
+            .addZombie(5, 13000, 4500)
+            .addZombie(6, 14000, 4500)
+            .addZombie(7, 15000, 3500)
+            .addZombie(8, 14500, 2500)
+            .addZombie(9, 15900, 500)
+            .state;
     }
 
     static public class GameTask extends RecursiveTask<Integer> {
         final private GameState state;
         final private Config config;
 
-        private GameTask(GameState start, Config config) {
+        public GameTask(GameState start, Config config) {
             this.config = config;
             this.state = start.clone();
         }
@@ -67,10 +95,61 @@ public class GameServer {
             Player player = new Player(state.clone(), new InternalCommunicator(), config);
 
             while (!state.isTerminal()) {
-                Game.process(state, ForkJoinTask.adapt(() -> player.process()).fork().join());
+                Game.process(state, ForkJoinTask.adapt(player::process).fork().join());
             }
 
             return state.score;
+        }
+    }
+
+    static public class ParallelGameTask extends RecursiveTask<List<Integer>> {
+        final private GameState[] states;
+        final private Config config;
+        final private int from;
+        final private int to;
+
+        public ParallelGameTask(Config config, GameState state, int count) {
+            this(config, IntStream.range(0, count).mapToObj(v -> state).toArray(GameState[]::new));
+        }
+
+        public ParallelGameTask(Config config, GameState... states) {
+            this(config, states, 0, states.length);
+        }
+
+        private ParallelGameTask(Config config, GameState[] states, int from, int to) {
+            this.config = config;
+            this.states = states;
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        protected List<Integer> compute() {
+            int length = to - from;
+
+            if (length == 0) {
+                return Collections.emptyList();
+            }
+            else if (length == 1) {
+                return Collections.singletonList(
+                    new GameTask(states[0], config).compute()
+                );
+            }
+            else {
+                List<Integer> results = new LinkedList<>();
+
+                int leftCount = length / 2;
+
+                ParallelGameTask leftTask = new ParallelGameTask(config, states, from, from + leftCount);
+                ParallelGameTask rightTask = new ParallelGameTask(config, states, from + leftCount, to);
+
+                rightTask.fork();
+
+                results.addAll(leftTask.compute());
+                results.addAll(rightTask.join());
+
+                return results;
+            }
         }
     }
 }
