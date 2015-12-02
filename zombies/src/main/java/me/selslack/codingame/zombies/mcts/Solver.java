a4a4942e27a4b8e64a128db4edf0f1bd38b01587
selslack <selslack@users.noreply.github.com>
2015-12-02 12:21:56 +0300

    Solver optimization commit for history

--- zombies/src/main/java/me/selslack/codingame/zombies/mcts/Solver.java
+++ zombies/src/main/java/me/selslack/codingame/zombies/mcts/Solver.java
@@ -2,6 +2,7 @@ package me.selslack.codingame.zombies.mcts;
 
 import me.selslack.codingame.zombies.Game;
 import me.selslack.codingame.zombies.GameState;
+import me.selslack.codingame.zombies.Human;
 import me.selslack.codingame.zombies.Waypoint;
 
 import java.util.*;
@@ -11,18 +12,23 @@ public class Solver {
 
     private Node root;
     final private Config config;
-
-    public int playouts = 0;
+    final private List<WaypointGenerator> generators;
 
     public Solver(GameState state, Config config) {
-        this.root = new Node(null, state.clone(), getPossibleMoves(state));
+        this.generators = new LinkedList<>();
+        this.generators.add(new FlexibleWaypointGenerator(config.firstFlexCircleDistance, config.firstFlexCircleCount));
+//        this.generators.add(new FlexibleWaypointGenerator(config.secondFlexCircleDistance, config.secondFlexCircleCount));
+//        this.generators.add(new FlexibleWaypointGenerator(150, 3));
+//        this.generators.add(new HumansWaypointGenerator());
+//        this.generators.add(new ZombiesWaypointGenerator());
+
+        this.root = new Node(null, state.clone(), getPossibleMoves(state, generators));
         this.config = config;
+
     }
 
     public Waypoint run() {
         long startTime = System.currentTimeMillis();
-
-        playouts = 0;
 
         while (System.currentTimeMillis() - startTime < config.timeLimit) {
             Node node = root;
@@ -42,12 +48,20 @@ public class Solver {
                     node.undiscovered.remove(0)
                 );
 
-                node = new Node(node, newState, getPossibleMoves(newState));
+                if (node.undiscovered.isEmpty()) {
+                    node.undiscovered = Collections.EMPTY_LIST;
+
+                    if (node.parent != null && node.parent.parent != null) {
+                        node.state = null;
+                    }
+                }
+
+                node = new Node(node, newState, getPossibleMoves(newState, generators));
             }
 
             int playoutScore = node.state.isTerminal()
                 ? node.score
-                : playout(node.state.clone(), config.playoutDepth);
+                : playout(node.state.clone(), generators, config.playoutDepth);
 
             // Backpropagate
             while (node != null) {
@@ -56,27 +70,25 @@ public class Solver {
 
                 node = node.parent;
             }
-
-            playouts++;
         }
 
-        System.out.println(playouts);
-
         root = root.children.stream()
-            .sorted(Comparator.comparingInt((Node v) -> v.score).thenComparingInt(v -> v.visits).reversed())
+            .sorted(Comparator.comparingInt((Node v) -> -v.score))
             .findFirst()
             .orElse(root);
 
-        return new Waypoint(root.state.getAsh());
+        root.parent = null;
+
+        return new Waypoint(root.ash);
     }
 
-    static public int playout(GameState state, int plyLimit) {
+    static public int playout(GameState state, List<WaypointGenerator> generators, int plyLimit) {
         for (int i = 0; i < plyLimit; i++) {
             if (state.isTerminal()) {
                 break;
             }
 
-            List<Waypoint> actions = getPossibleMoves(state);
+            List<Waypoint> actions = getPossibleMoves(state, generators);
             Waypoint action = actions.get(rnd.nextInt(actions.size()));
 
             Game.process(state, action);
@@ -85,15 +97,16 @@ public class Solver {
         return state.score;
     }
 
-    static public List<Waypoint> getPossibleMoves(GameState state) {
-        if (state.isTerminal()) {
+    static public List<Waypoint> getPossibleMoves(GameState state, List<WaypointGenerator> generators) {
+        if (state.isTerminal() || generators.isEmpty()) {
             return Collections.EMPTY_LIST;
         }
 
-        ArrayList<Waypoint> result = new ArrayList<>(64);
+        ArrayList<Waypoint> result = new ArrayList<>(32);
 
-        result.addAll(new HumansWaypointGenerator().generate(state));
-        result.addAll(new ZombiesWaypointGenerator().generate(state));
+        for (WaypointGenerator generator : generators) {
+            result.addAll(generator.generate(state));
+        }
 
         return result;
     }
@@ -102,16 +115,18 @@ public class Solver {
         public int score = 0;
         public int visits = 0;
         public Node parent;
+        public GameState state;
+        public List<Waypoint> undiscovered;
 
-        final public List<Waypoint> undiscovered;
         final public List<Node> children;
-        final public GameState state;
+        final public Human ash;
 
         private Node(Node parent, GameState state, List<Waypoint> undiscovered) {
             this.parent = parent;
             this.state = state;
             this.undiscovered = undiscovered;
             this.children = new LinkedList<>();
+            this.ash = state.getAsh();
 
             if (parent != null) {
                 parent.children.add(this);
@@ -120,7 +135,7 @@ public class Solver {
 
         @Override
         public String toString() {
-            return "Node{score=" + score + ", visits=" + visits + ", point=" + new Waypoint(state.getAsh()) + "}";
+            return "Node{score=" + score + ", visits=" + visits + ", point=" + new Waypoint(ash) + "}";
         }
     }
 }
